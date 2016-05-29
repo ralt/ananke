6e50447104f2405ecd2d7b045a36fc4d0fe18deb
Florian MARGAINE <florian@margaine.com>
2016-05-29 10:03:53 +0200

    fixit

--- ananke/main.py
+++ ananke/main.py
@@ -1,8 +1,9 @@
 import os
 import sys
 import tty
-from subprocess import Popen
-from select import select
+import termios
+import select
+import subprocess
 
 
 def main():
@@ -11,9 +12,10 @@ def main():
     stdout = sys.stdout.fileno()
 
     try:
+        old_settings = termios.tcgetattr(sys.stdout)
         tty.setraw(sys.stdout)
 
-        process = Popen(
+        process = subprocess.Popen(
             args=["/bin/bash"],
             stdin=slave,
             stdout=slave,
@@ -26,12 +28,13 @@ def main():
             if process.poll() is not None:
                 break
 
-            r, _, _ = select([master, sys.stdin], [], [], 0.2)
+            r, _, _ = select.select([master, sys.stdin], [], [], 0.2)
 
             if master in r:
                 os.write(stdout, os.read(master, 1024))
             if sys.stdin in r:
                 os.write(master, os.read(stdin, 1024))
     finally:
+        termios.tcsetattr(sys.stdout, termios.TCSADRAIN, old_settings)
         os.close(master)
         os.close(slave)
